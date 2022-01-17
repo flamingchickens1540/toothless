3a8914d4b4d5b886fa0623f23a6768b53dcf63a3
Zach R <92497727+rutmanz@users.noreply.github.com>
2022-01-17 14:44:58 -0800

    feat: create test auto drive command

---
+++ src/main/java/org/team1540/robot2022/commands/drivetrain/AutoTest.java
@@ -0,0 +1,76 @@
+package org.team1540.robot2022.commands.drivetrain;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import org.team1540.robot2022.RamseteConfig;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryUtil;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
+
+public class AutoTest extends CommandBase {
+    private String trajectoryJSON = "paths/AutoTest.wpilib.json";
+    private Trajectory trajectory;
+
+    private RamseteCommand command;
+
+    private DriveTrain driveTrain;
+
+    public AutoTest(DriveTrain driveTrain) {
+        try {
+            Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
+            trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+        } catch (IOException ex) {
+            DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
+        }
+
+        this.driveTrain = driveTrain;
+        getRamseteCommand(trajectory);
+        addRequirements(driveTrain);
+    }
+
+    private RamseteCommand getRamseteCommand(Trajectory trajectory) {
+        return new RamseteCommand(
+                trajectory,
+                driveTrain::getPose,
+                new RamseteController(RamseteConfig.kRamseteB, RamseteConfig.kRamseteZeta),
+                new SimpleMotorFeedforward(
+                        RamseteConfig.ksVolts,
+                        RamseteConfig.kvVoltSecondsPerMeter,
+                        RamseteConfig.kaVoltSecondsSquaredPerMeter),
+                RamseteConfig.kDriveKinematics,
+                driveTrain::getWheelSpeeds,
+                new PIDController(RamseteConfig.kPDriveVel, 0, 0),
+                new PIDController(RamseteConfig.kPDriveVel, 0, 0),
+                // RamseteCommand passes volts to the callback
+                driveTrain::tankDriveVolts,
+                driveTrain);
+    }
+
+    @Override
+    public void initialize() {
+        command.initialize();
+    }
+
+    @Override
+    public void execute() {
+        command.execute();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        command.end(interrupted);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return command.isFinished();
+    }
+}
--- src/main/java/org/team1540/robot2022/commands/drivetrain/DriveTrain.java
+++ src/main/java/org/team1540/robot2022/commands/drivetrain/DriveTrain.java
@@ -2,10 +2,14 @@ package org.team1540.robot2022.commands.drivetrain;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
-import org.team1540.robot2022.Constants.DriveTrain.Motors;
+import org.team1540.robot2022.Constants.DriveConstants.Motors;
+import org.team1540.robot2022.utils.ChickenTalonFX;
+import org.team1540.robot2022.utils.NavX;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrain extends SubsystemBase {
@@ -16,8 +20,12 @@ public class DriveTrain extends SubsystemBase {
     private final ChickenTalonFX[] driveL = { driveLFront, driveLRear };
     private final ChickenTalonFX[] driveR = { driveRFront, driveRRear };
     private final ChickenTalonFX[] driveMotors = { driveLFront, driveLRear, driveRFront, driveRRear };
+    private final NavX navx;
+    private final DifferentialDriveOdometry driveOdometry;
 
     public DriveTrain(NeutralMode brakeType, NavX navx) {
+        driveOdometry = new DifferentialDriveOdometry(navx.getRotation2d());
+        this.navx = navx;
         for (ChickenTalonFX motor : driveMotors) {
             motor.configFactoryDefault();
             motor.setNeutralMode(brakeType);
@@ -34,6 +42,42 @@ public class DriveTrain extends SubsystemBase {
         driveRRear.follow(driveRFront);
     }
 
+    @Override
+    public void periodic() {
+        driveOdometry.update(
+                navx.getRotation2d(),
+                driveLFront.getDistanceMeters(),
+                driveRFront.getDistanceMeters());
+    }
+
+    public void resetEncoders() {
+        for (ChickenTalonFX motor : driveMotors) {
+            motor.setSelectedSensorPosition(0);
+        }
+    }
+
+    /**
+     * Returns the current wheel speeds of the robot.
+     * 
+     * @return The current wheel speeds.
+     */
+    public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+        return new DifferentialDriveWheelSpeeds(
+                driveLFront.getRateMetersPerSecond(),
+                driveRFront.getRateMetersPerSecond());
+
+    }
+
+    /**
+     * Returns the currently-estimated pose of the robot.
+     *
+     * @return The pose.
+     */
+    public Pose2d getPose() {
+        return driveOdometry.getPoseMeters();
+
+    }
+
     public void setPercent(double leftPercent, double rightPercent) {
         driveLFront.set(ControlMode.PercentOutput, leftPercent);
         driveRFront.set(ControlMode.PercentOutput, rightPercent);
@@ -43,5 +87,11 @@ public class DriveTrain extends SubsystemBase {
         for (ChickenTalonFX motor : driveMotors) {
             motor.setNeutralMode(mode);
         }
+    }
+
+    public void tankDriveVolts(double leftVolts, double rightVolts) {
+        driveLFront.setVoltage(leftVolts);
+        driveRFront.setVoltage(rightVolts);
+
     }
 }
