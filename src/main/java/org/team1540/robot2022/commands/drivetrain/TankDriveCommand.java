4f5a0d7b53220767b9f7b508f8ff07f090eba07f
Zach R <92497727+rutmanz@users.noreply.github.com>
2022-02-03 17:43:15 -0800

    feat: add SlewRateLimiter to tank drive - causes some issues with the robot driving after controls are released (trying to ramp down slowly)

--- src/main/java/org/team1540/robot2022/commands/drivetrain/TankDriveCommand.java
+++ src/main/java/org/team1540/robot2022/commands/drivetrain/TankDriveCommand.java
@@ -1,5 +1,8 @@
 package org.team1540.robot2022.commands.drivetrain;
 
+import org.team1540.robot2022.Constants.DriveConstants;
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
@@ -7,7 +10,9 @@ public class TankDriveCommand extends CommandBase {
     private final double deadzone = 0.1;
     private final DriveTrain drivetrain;
     private final XboxController controller;
-    private final double speedModifier = 0.5;
+    private final double speedModifier = 1;
+    private final SlewRateLimiter leftRateLimiter = new SlewRateLimiter(DriveConstants.tankDriveAccellerationLimit);
+    private final SlewRateLimiter rightRateLimiter = new SlewRateLimiter(DriveConstants.tankDriveAccellerationLimit);
 
     public TankDriveCommand(DriveTrain drivetrain, XboxController controller) {
         this.drivetrain = drivetrain;
@@ -24,8 +29,8 @@ public class TankDriveCommand extends CommandBase {
     }
 
     public void execute() {
-        double valueL = -speedModifier * applyDeadzone(controller.getLeftY());
-        double valueR = -speedModifier * applyDeadzone(controller.getRightY());
+        double valueL = leftRateLimiter.calculate(-speedModifier * applyDeadzone(controller.getLeftY()));
+        double valueR = rightRateLimiter.calculate(-speedModifier * applyDeadzone(controller.getRightY()));
         drivetrain.setPercent(valueL, valueR);
     }
 }
