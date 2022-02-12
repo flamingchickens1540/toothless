8f55b5d823e971e5d9410a0e7ce6755d5b9c250b
Nate Sales <nate@natesales.net>
2022-02-11 21:50:48 -0800

    refactor: cleanup shooter

--- src/main/java/org/team1540/robot2022/commands/shooter/Shooter.java
+++ src/main/java/org/team1540/robot2022/commands/shooter/Shooter.java
@@ -1,43 +1,31 @@
 package org.team1540.robot2022.commands.shooter;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
-
 import edu.wpi.first.networktables.EntryListenerFlags;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import org.team1540.robot2022.LunaUtils.MotorConfigUtils;
+import org.team1540.robot2022.Constants;
 import org.team1540.robot2022.LunaUtils.Timer;
-;
-
-
-
-//Add commands 
 
 public class Shooter extends SubsystemBase {
-   //private ShooterTesting shooter = new ShooterTesting();
+    private final double kP = 0.5;
+    private final double kD = 30;
+    private final double kF = 0.0484;
+    private final double kI = 0;
+    private final double iZone = 0;
 
-    private double kP = 0.5;
-    private double kD = 30;
-    private double kF = 0.0484;
-    private double kI = 0;
-    private double iZone = 0;
+    public TalonFX shooterMotorFront = new TalonFX(Constants.ShooterConstants.front);
+    public TalonFX shooterMotorRear = new TalonFX(Constants.ShooterConstants.rear);
 
-    public TalonFX shooterMotorFront = new TalonFX(9);
-    public TalonFX shooterMotorRear = new TalonFX(10);
+    private final Timer kdTimer = new Timer();
 
-   
-    private Timer kdTimer = new Timer();
-
-    public Shooter(XboxController copilot) {
+    public Shooter() {
         setupFlywheelMotors();
         setupPIDs();
         updatePIDs();
@@ -50,9 +38,6 @@ public class Shooter extends SubsystemBase {
         Constants.ShooterConstants.currentLimitConfig.applyTo(shooterMotorRear);
         shooterMotorFront.setNeutralMode(NeutralMode.Coast);
         shooterMotorRear.setNeutralMode(NeutralMode.Coast);
-
-
-
         shooterMotorRear.setInverted(TalonFXInvertType.OpposeMaster);
     }
 
@@ -63,77 +48,55 @@ public class Shooter extends SubsystemBase {
         SmartDashboard.putNumber("shooter/tuning/kF", kF);
         SmartDashboard.putNumber("shooter/tuning/kD", kD);
 
-
         NetworkTableInstance.getDefault().getTable("SmartDashboard/shooter/tuning").addEntryListener((table, key, entry, value, flags) -> updatePIDs(), EntryListenerFlags.kUpdate);
     }
 
     @Override
     public void periodic() {
         SmartDashboard.putNumber("shooter/current", shooterMotorFront.getStatorCurrent() + shooterMotorRear.getStatorCurrent());
-        SmartDashboard.putNumber("shooter/velocityA", getVelocityRPMA());
-        SmartDashboard.putNumber("shooter/velocityB", getVelocityRPMB());
+        SmartDashboard.putNumber("shooter/velocityFront", getFrontVelocityRPM());
+        SmartDashboard.putNumber("shooter/velocityRear", getRearVelocityRPM());
         SmartDashboard.putNumber("shooter/error", getClosedLoopError());
-        SmartDashboard.putNumber("setVelocityRPMA", setVelocityRPMA(500));
-        SmartDashboard.putNumber("setVelocityRPMB", setVelocityRPMB(500));
-
+        SmartDashboard.putNumber("setFrontVelocityRPM", setFrontVelocityRPM(500));
+        SmartDashboard.putNumber("setRearVelocityRPM", setRearVelocityRPM(500));
     }
-
 
     public void stop() {
         shooterMotorFront.set(TalonFXControlMode.PercentOutput, 0);
         shooterMotorRear.set(TalonFXControlMode.PercentOutput, 0);
     }
 
-    public double getVelocityRPMA() {
+    public double getFrontVelocityRPM() {
         return (shooterMotorFront.getSelectedSensorVelocity() / 2048.0) * 600;
     }
-    public double getVelocityRPMB() {
+
+    public double getRearVelocityRPM() {
         return (shooterMotorRear.getSelectedSensorVelocity() / 2048.0) * 600;
     }
 
-    public double setVelocityRPMA(double velocity) {
+    public double setFrontVelocityRPM(double velocity) {
         shooterMotorFront.set(TalonFXControlMode.Velocity, (velocity * 2048.0) / 600);
-        return velocity; 
+        return velocity;
     }
-    public double setVelocityRPMB(double velocity) {
-        shooterMotorFront.set(TalonFXControlMode.Velocity, (velocity * 2048.0) / 600);
-        return velocity; 
+
+    public double setRearVelocityRPM(double velocity) {
+        shooterMotorRear.set(TalonFXControlMode.Velocity, (velocity * 2048.0) / 600);
+        return velocity;
     }
 
     private void updatePIDs() {
         shooterMotorFront.config_kP(0, SmartDashboard.getNumber("shooter/tuning/kP", kP));
         shooterMotorFront.config_kI(0, SmartDashboard.getNumber("shooter/tuning/kI", kI));
+        shooterMotorFront.config_kD(0, SmartDashboard.getNumber("shooter/tuning/kI", kD));
         shooterMotorFront.config_kF(0, SmartDashboard.getNumber("shooter/tuning/kF", kF));
         shooterMotorFront.config_IntegralZone(0, (int) SmartDashboard.getNumber("shooter/tuning/iZone", iZone));
-    }
-
-    public void setPercent(double value) {
-        shooterMotorFront.set(ControlMode.PercentOutput, value);
-        shooterMotorRear.set(ControlMode.PercentOutput, value);
-
     }
 
     public double getClosedLoopError() {
         return shooterMotorFront.getClosedLoopError() + shooterMotorRear.getClosedLoopError();
     }
 
-    public void config_kD(double kD) {
-        shooterMotorFront.config_kD(0, kD);
-        shooterMotorRear.config_kD(0, kD);
-
-    }
-
-    public void setShooterMotorA(){
-
-    }
-
     public Command commandStop() {
         return new InstantCommand(this::stop, this);
     }
-
-   
-    
-    
 }
-
-
--- src/main/java/org/team1540/robot2022/commands/shooter/VelocitySetCommand.java
+++ src/main/java/org/team1540/robot2022/commands/shooter/VelocitySetCommand.java
@@ -1,36 +1,36 @@
 package org.team1540.robot2022.commands.shooter;
 
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import org.team1540.robot2022.utils.InterpolationTable;
 import org.team1540.robot2022.utils.Limelight;
-import org.team1540.robot2022.utils.LinearInterpolator;
-
-import edu.wpi.first.math.interpolation.Interpolatable;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class VelocitySetCommand extends CommandBase {
     private final Shooter shooter;
-    private Limelight limeLight;
-    private double velocity;
-    private InterpolationTable interpolationTable; 
-    public  VelocitySetCommand(Shooter shooter, InterpolationTable interpolationTable, Limelight limelight){
-        this.shooter = shooter; 
-        this.interpolationTable = interpolationTable; 
-        this.limeLight = limeLight; 
-        addRequirements(shooter); 
-    }
-    
-    @Override
-    public void initialize(){
+    private final InterpolationTable interpolationTable;
+    private final Limelight limelight;
 
+    public VelocitySetCommand(Shooter shooter, InterpolationTable interpolationTable, Limelight limelight) {
+        this.shooter = shooter;
+        this.interpolationTable = interpolationTable;
+        this.limelight = limelight;
+        addRequirements(shooter);
     }
 
     @Override
-    public void execute(){
-                shooter.shooterMotorFront.set(TalonFXControlMode.Velocity, (interpolationTable.frontFlywheelInterpolator.getInterpolatedValue(limeLight.getCalculatedDistance()) * 2048.0) / 600);
-                shooter.shooterMotorRear.set(TalonFXControlMode.Velocity, (interpolationTable.rearFlywheelInterpolator.getInterpolatedValue(limeLight.getCalculatedDistance()) * 2048.0) / 600);
+    public void initialize() {
+    }
 
+    @Override
+    public void execute() {
+        shooter.shooterMotorFront.set(
+            TalonFXControlMode.Velocity,
+            (interpolationTable.frontFlywheelInterpolator.getInterpolatedValue(limelight.getCalculatedDistance()) * 2048.0) / 600
+        );
+
+        shooter.shooterMotorRear.set(
+            TalonFXControlMode.Velocity,
+            (interpolationTable.rearFlywheelInterpolator.getInterpolatedValue(limelight.getCalculatedDistance()) * 2048.0) / 600
+        );
     }
 }
