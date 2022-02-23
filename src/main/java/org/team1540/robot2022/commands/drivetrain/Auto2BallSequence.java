7b4905f8196b3c56efa7b8589072df0eb597dba6
Nate Sales <nate@natesales.net>
2022-02-22 16:30:14 -0800

    refactor: reformat

--- src/main/java/org/team1540/robot2022/commands/drivetrain/Auto2BallSequence.java
+++ src/main/java/org/team1540/robot2022/commands/drivetrain/Auto2BallSequence.java
@@ -14,16 +14,17 @@ public class Auto2BallSequence extends SequentialCommandGroup {
 
     /**
      * Constructs a new Auto2Ball Sequence
+     *
      * @param drivetrain The drivetrain subsystem (For driving)
-     * @param intake The intake subsystem (For collecting balls)
-     * @param indexer The indexer subsystem (For collecting balls and ShootSequence)
-     * @param shooter The shooter subsystem (For ShootSequence)
-     * @param limelight The limelight (For PointToTarget)
-     * @param isPosA If the robot is positioned in the upper right or bottom left starting position
+     * @param intake     The intake subsystem (For collecting balls)
+     * @param indexer    The indexer subsystem (For collecting balls and ShootSequence)
+     * @param shooter    The shooter subsystem (For ShootSequence)
+     * @param limelight  The limelight (For PointToTarget)
+     * @param isPosA     If the robot is positioned in the upper right or bottom left starting position
      */
     public Auto2BallSequence(Drivetrain drivetrain, Intake intake, Indexer indexer, Shooter shooter, Hood hood, Limelight limelight, boolean isPosA, Command indexCommand) {
-        String trajectoryFile = "2ball.pos"+(isPosA?"A":"B")+".path1.wpilib.json";
-        addCommands( 
+        String trajectoryFile = "2ball.pos" + (isPosA ? "A" : "B") + ".path1.wpilib.json";
+        addCommands(
                 AutoSequenceWrapper.runPath(drivetrain, intake, indexer, trajectoryFile),  // Follow the path to collect the first ball
                 new ShootSequence(shooter, indexer, drivetrain, hood, intake, limelight, indexCommand)         // Shoot the 2 indexed balls (starts with one, collects one)
         );
