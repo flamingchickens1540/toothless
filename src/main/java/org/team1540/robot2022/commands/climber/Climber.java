a6c9824b21da77b38e29c6368dfd1d5f3684b9c9
Nate Sales <nate@natesales.net>
2022-02-27 20:33:04 -0800

    refactor: use DoubleSolenoid for climber

--- src/main/java/org/team1540/robot2022/commands/climber/Climber.java
+++ src/main/java/org/team1540/robot2022/commands/climber/Climber.java
@@ -3,8 +3,8 @@ package org.team1540.robot2022.commands.climber;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.team1540.robot2022.Constants;
@@ -16,8 +16,12 @@ public class Climber extends SubsystemBase {
     private final ChickenTalonFX motorRight = new ChickenTalonFX(ClimberConstants.Motors.RIGHT);
     private final ChickenTalonFX[] motors = new ChickenTalonFX[]{motorLeft, motorRight};
 
-    private final Solenoid solenoidA = new Solenoid(Constants.PNEUMATIC_HUB, PneumaticsModuleType.REVPH, ClimberConstants.Solenoids.SOLENOID_A);
-    private final Solenoid solenoidB = new Solenoid(Constants.PNEUMATIC_HUB, PneumaticsModuleType.REVPH, ClimberConstants.Solenoids.SOLENOID_B);
+    private final DoubleSolenoid solenoid = new DoubleSolenoid(
+            Constants.PNEUMATIC_HUB,
+            PneumaticsModuleType.REVPH,
+            ClimberConstants.Solenoids.SOLENOID_A,
+            ClimberConstants.Solenoids.SOLENOID_B
+    );
 
     public Climber() {
         Constants.ShooterConstants.CURRENT_LIMIT_CONFIG.applyTo(new TalonFX[]{motorLeft, motorRight});
@@ -33,13 +37,13 @@ public class Climber extends SubsystemBase {
      * @param forward If the solenoids should be extended forward
      */
     public void setSolenoids(boolean forward) {
-        solenoidA.set(forward);
-        solenoidB.set(!forward);
+        solenoid.set(forward ? DoubleSolenoid.Value.kForward : DoubleSolenoid.Value.kReverse);
     }
 
     public void setLeftPercent(double percent) {
         motorLeft.setPercent(percent);
     }
+
     public void setRightPercent(double percent) {
         motorRight.setPercent(percent);
     }
@@ -63,9 +67,12 @@ public class Climber extends SubsystemBase {
 
     /**
      * Sets the NeutralMode for the climber (either coast or brake)
+     *
      * @param mode The mode to set the wheels to
      */
     public void setNeutralMode(NeutralMode mode) {
-        for (ChickenTalonFX motor : motors) { motor.setNeutralMode(mode); }
+        for (ChickenTalonFX motor : motors) {
+            motor.setNeutralMode(mode);
+        }
     }
 }
