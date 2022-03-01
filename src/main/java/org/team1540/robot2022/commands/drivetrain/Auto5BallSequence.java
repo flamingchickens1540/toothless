600ea784baca240c4212b118ef635fd6444b4c14
Zach R <92497727+rutmanz@users.noreply.github.com>
2022-03-01 15:40:27 -0800

    feat: add 5-ball auto

---
+++ src/main/java/org/team1540/robot2022/commands/drivetrain/Auto5BallSequence.java
@@ -0,0 +1,30 @@
+package org.team1540.robot2022.commands.drivetrain;
+
+import org.team1540.robot2022.commands.hood.Hood;
+import org.team1540.robot2022.commands.indexer.Indexer;
+import org.team1540.robot2022.commands.intake.Intake;
+import org.team1540.robot2022.commands.shooter.ShootSequence;
+import org.team1540.robot2022.commands.shooter.Shooter;
+import org.team1540.robot2022.utils.AutoHelper;
+import org.team1540.robot2022.utils.Limelight;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class Auto5BallSequence extends SequentialCommandGroup {
+    public Auto5BallSequence(Drivetrain drivetrain, Intake intake, Indexer indexer, Shooter shooter, Hood hood, Limelight limelight) {
+        addCommands(
+                AutoHelper.runPath(drivetrain, intake, indexer, shooter, "2ball.posA.path1.wpilib.json"), // Path follow to collect second ball
+                new ShootSequence(shooter, indexer, drivetrain, hood, intake, limelight),        // Shoot the 2 indexed balls (starts with one, collects one)
+                AutoHelper.runPath(drivetrain, intake, indexer, shooter, "5ball.posA.path2.wpilib.json"), // Path follow to collect third and fourth balls
+                new ConditionalCommand(                                                          // If (2 balls are indexed) then shoot, else keep going
+                    new ShootSequence(shooter, indexer, drivetrain, hood, intake, limelight), 
+                    new PrintCommand("1 ball indexed; skipping shot"), 
+                    indexer::isFull
+                ),
+                AutoHelper.runPath(drivetrain, intake, indexer, shooter, "5ball.posA.path3.wpilib.json"), // Path follow to collect fifth ball
+                new ShootSequence(shooter, indexer, drivetrain, hood, intake, limelight)         // Shoot the fifth ball
+        );
+    }
+}
+
