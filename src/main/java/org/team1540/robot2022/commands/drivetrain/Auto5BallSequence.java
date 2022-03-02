21bbd2e30c7aadd1bf3bdd129ea28ceaf17889b2
Zach R <92497727+rutmanz@users.noreply.github.com>
2022-03-01 16:00:22 -0800

    fix: add lidar to Auto5BallSequence

--- src/main/java/org/team1540/robot2022/commands/drivetrain/Auto5BallSequence.java
+++ src/main/java/org/team1540/robot2022/commands/drivetrain/Auto5BallSequence.java
@@ -6,24 +6,25 @@ import org.team1540.robot2022.commands.intake.Intake;
 import org.team1540.robot2022.commands.shooter.ShootSequence;
 import org.team1540.robot2022.commands.shooter.Shooter;
 import org.team1540.robot2022.utils.AutoHelper;
+import org.team1540.robot2022.utils.LIDAR;
 import org.team1540.robot2022.utils.Limelight;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class Auto5BallSequence extends SequentialCommandGroup {
-    public Auto5BallSequence(Drivetrain drivetrain, Intake intake, Indexer indexer, Shooter shooter, Hood hood, Limelight limelight) {
+    public Auto5BallSequence(Drivetrain drivetrain, Intake intake, Indexer indexer, Shooter shooter, Hood hood, Limelight limelight, LIDAR lidar) {
         addCommands(
                 AutoHelper.runPath(drivetrain, intake, indexer, shooter, "2ball.posA.path1.wpilib.json"), // Path follow to collect second ball
-                new ShootSequence(shooter, indexer, drivetrain, hood, intake, limelight),        // Shoot the 2 indexed balls (starts with one, collects one)
+                new ShootSequence(shooter, indexer, drivetrain, hood, intake, limelight, lidar),        // Shoot the 2 indexed balls (starts with one, collects one)
                 AutoHelper.runPath(drivetrain, intake, indexer, shooter, "5ball.posA.path2.wpilib.json"), // Path follow to collect third and fourth balls
                 new ConditionalCommand(                                                          // If (2 balls are indexed) then shoot, else keep going
-                    new ShootSequence(shooter, indexer, drivetrain, hood, intake, limelight), 
+                    new ShootSequence(shooter, indexer, drivetrain, hood, intake, limelight, lidar), 
                     new PrintCommand("1 ball indexed; skipping shot"), 
                     indexer::isFull
                 ),
                 AutoHelper.runPath(drivetrain, intake, indexer, shooter, "5ball.posA.path3.wpilib.json"), // Path follow to collect fifth ball
-                new ShootSequence(shooter, indexer, drivetrain, hood, intake, limelight)         // Shoot the fifth ball
+                new ShootSequence(shooter, indexer, drivetrain, hood, intake, limelight, lidar)         // Shoot the fifth ball
         );
     }
 }
