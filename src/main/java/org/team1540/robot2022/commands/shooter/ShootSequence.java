33ed0cb6278f84ddcec245aec94dbdf62c7b59a8
Nate Sales <nate@natesales.net>
2022-03-03 17:01:34 -0800

    fix: bypass all complicated shooter logic and set shooter profile to FAR

--- src/main/java/org/team1540/robot2022/commands/shooter/ShootSequence.java
+++ src/main/java/org/team1540/robot2022/commands/shooter/ShootSequence.java
@@ -11,9 +11,8 @@ import org.team1540.robot2022.commands.intake.Intake;
 import org.team1540.robot2022.utils.FeatherClient;
 import org.team1540.robot2022.utils.LIDAR;
 import org.team1540.robot2022.utils.Limelight;
-import org.team1540.robot2022.utils.RepeatCommand;
 
-public class ShootSequence extends ParallelCommandGroup {
+public class ShootSequence extends SequentialCommandGroup {
     private final Shooter shooter;
     private final Indexer indexer;
     private final Limelight limelight;
@@ -31,85 +30,85 @@ public class ShootSequence extends ParallelCommandGroup {
         this.shooter = shooter;
         this.indexer = indexer;
         this.limelight = limelight;
-        this.profile = profile;
+//        this.profile = profile;
+        this.profile = Shooter.ShooterProfile.FAR;
 
         addRequirements(shooter, indexer, drivetrain);
         addCommands(
-                deadline(
-                        sequence(
-                                new ConditionalCommand( // Shoot if target isn't found, otherwise lineup and shoot
-                                        new PointToTarget(drivetrain, limelight).withTimeout(2),
-                                        new InstantCommand(() -> {
-                                            if (!shootWithoutTarget) {
-                                                this.end(true);
-                                            }
-                                        }),
-                                        limelight::isTargetFound
-                                ),
-                                new WaitCommand(1),
-                                new WaitUntilCommand(shooter::isSpunUp),
-                                indexer.commandStop(),
-                                indexer.commandSet(Indexer.IndexerState.FORWARD_FULL, Indexer.IndexerState.OFF), // Run top indexer
-                                new WaitUntilCommand(() -> !indexer.getTopSensor()),
-                                new WaitCommand(SmartDashboard.getNumber("shooter/tuning/waitAfterFirstBall", 1)), // Wait for top ball to leave and shooter to recover
+                new PrintCommand("Starting shoot sequence with profile " + this.profile.toString()),
+                new InstantCommand(() -> limelight.setLeds(true)),
+                new WaitCommand(0.2), // Wait for limelight to acquire target after LEDs turn on
+                new InstantCommand(() -> {
+//                    limelightDistance = limelight.getCalculatedDistance();
+//                    lidarDistance = lidar.getDistance();
+//
+//                    // Positional argument shooter, not the field
+//                    if (profile == Shooter.ShooterProfile.AUTOMATIC) {
+//                        if (lidarDistance < SmartDashboard.getNumber("shooter/profiles/maxHub", 10)) {
+//                            this.profile = Shooter.ShooterProfile.HUB;
+//                        } else if (lidarDistance < SmartDashboard.getNumber("shooter/profiles/maxTarmac", 93)) {
+//                            this.profile = Shooter.ShooterProfile.TARMAC;
+//                        } else {
+//                            this.profile = Shooter.ShooterProfile.FAR;
+//                        }
+//                    }
 
-                                new WaitUntilCommand(shooter::isSpunUp),
-                                indexer.commandSet(Indexer.IndexerState.FORWARD_FULL, Indexer.IndexerState.FORWARD_FULL), // Run bottom indexer to shoot bottom ball
-                                new WaitUntilCommand(() -> !indexer.getTopSensor()),
-                                new WaitCommand(2), // Wait for bottom ball to leave
-                                indexer.commandSet(Indexer.IndexerState.OFF, Indexer.IndexerState.OFF),
-                                shooter.commandStop(),
-                                new InstantCommand(() -> FeatherClient.recordShot(limelightDistance, lidarDistance, frontVelocity, rearVelocity, hoodState, profile))
-                        ),
-                        sequence(
-                                new InstantCommand(() -> limelight.setLeds(true)),
-                                new WaitCommand(0.2), // Wait for limelight to acquire target after LEDs turn on
-                                new RepeatCommand(() -> {
-                                    limelightDistance = limelight.getCalculatedDistance();
-                                    lidarDistance = lidar.getDistance();
+                    if (this.profile == Shooter.ShooterProfile.FAR) {
+                        hoodState = true;
+                        frontVelocity = interpolationTable.frontFlywheelInterpolator.getInterpolatedValue(limelightDistance);
+                        rearVelocity = interpolationTable.rearFlywheelInterpolator.getInterpolatedValue(limelightDistance);
+                    } else if (this.profile == Shooter.ShooterProfile.HUB) {
+                        hoodState = false;
+                        frontVelocity = InterpolationTable.hubFront;
+                        rearVelocity = InterpolationTable.hubRear;
+                    } else if (this.profile == Shooter.ShooterProfile.TARMAC) {
+                        hoodState = false;
+                        frontVelocity = InterpolationTable.tarmacFront;
+                        rearVelocity = InterpolationTable.tarmacRear;
+                    } else if (this.profile == Shooter.ShooterProfile.LOWGOAL) {
+                        hoodState = false;
+                        frontVelocity = InterpolationTable.lowGoalFront;
+                        rearVelocity = InterpolationTable.lowGoalRear;
+                    }
 
-                                    // Positional argument shooter, not the field
-                                    if (profile == Shooter.ShooterProfile.AUTOMATIC) {
-                                        if (lidarDistance < SmartDashboard.getNumber("shooter/profiles/maxHub", 10)) {
-                                            this.profile = Shooter.ShooterProfile.HUB;
-                                        } else if (lidarDistance < SmartDashboard.getNumber("shooter/profiles/maxTarmac", 93)) {
-                                            this.profile = Shooter.ShooterProfile.TARMAC;
-                                        } else {
-                                            this.profile = Shooter.ShooterProfile.FAR;
-                                        }
-                                    }
+                    // Used for tuning:
+                    // hoodState = true;
+                    // frontVelocity = SmartDashboard.getNumber("shooter/tuning/frontRPM", 0);
+                    // rearVelocity = SmartDashboard.getNumber("shooter/tuning/rearRPM", 0);
 
-                                    switch (this.profile) {
-                                        case FAR:
-                                            hoodState = true;
-                                            intake.setFold(true);
-                                            frontVelocity = interpolationTable.frontFlywheelInterpolator.getInterpolatedValue(limelightDistance);
-                                            rearVelocity = interpolationTable.rearFlywheelInterpolator.getInterpolatedValue(limelightDistance);
-                                        case HUB:
-                                            hoodState = false;
-                                            frontVelocity = InterpolationTable.hubFront;
-                                            rearVelocity = InterpolationTable.hubRear;
-                                        case TARMAC:
-                                            hoodState = false;
-                                            frontVelocity = InterpolationTable.tarmacFront;
-                                            rearVelocity = InterpolationTable.tarmacRear;
-                                        case LOWGOAL:
-                                            hoodState = false;
-                                            frontVelocity = InterpolationTable.lowGoalFront;
-                                            rearVelocity = InterpolationTable.lowGoalRear;
-                                    }
+                    hood.set(hoodState);
+                    shooter.setVelocityRPM(shooter.shooterMotorFront, frontVelocity);
+                    shooter.setVelocityRPM(shooter.shooterMotorRear, rearVelocity);
+                }),
 
-                                    // Used for tuning:
-                                    // hoodState = true;
-                                    // frontVelocity = SmartDashboard.getNumber("shooter/tuning/frontRPM", 0);
-                                    // rearVelocity = SmartDashboard.getNumber("shooter/tuning/rearRPM", 0);
+                new ConditionalCommand( // Shoot if target isn't found, otherwise lineup and shoot
+                        new PointToTarget(drivetrain, limelight).withTimeout(2),
+                        new InstantCommand(() -> {
+//                            if (!shootWithoutTarget) {
+//                                this.end(true);
+//                            }
+                        }),
+                        limelight::isTargetFound
+                ),
+                new WaitUntilCommand(shooter::isSpunUp),
+                indexer.commandStop(),
+                indexer.commandSet(Indexer.IndexerState.FORWARD_FULL, Indexer.IndexerState.OFF), // Run top indexer
+                new WaitUntilCommand(() -> !indexer.getTopSensor()),
+                new WaitCommand(SmartDashboard.getNumber("shooter/tuning/waitAfterFirstBall", 1)), // Wait for top ball to leave and shooter to recover
+                indexer.commandSet(Indexer.IndexerState.OFF, Indexer.IndexerState.OFF),
 
-                                    hood.set(hoodState);
-                                    shooter.setVelocityRPM(shooter.shooterMotorFront, frontVelocity);
-                                    shooter.setVelocityRPM(shooter.shooterMotorRear, rearVelocity);
-                                }, shooter)
-                        )
-                )
+                // Shoot second ball
+                new WaitCommand(1),
+                indexer.commandSet(Indexer.IndexerState.FORWARD, Indexer.IndexerState.FORWARD), // Move bottom ball up
+                new WaitUntilCommand(indexer::getTopSensor), // Stop when ball is up high TODO: Standby here?
+                indexer.commandSet(Indexer.IndexerState.OFF, Indexer.IndexerState.OFF),
+
+                new WaitUntilCommand(shooter::isSpunUp),
+                indexer.commandSet(Indexer.IndexerState.FORWARD_FULL, Indexer.IndexerState.FORWARD_FULL), // Run bottom indexer to shoot bottom ball
+                new WaitUntilCommand(() -> !indexer.getTopSensor()).andThen(new WaitCommand(2)), // Wait for bottom ball to leave
+                indexer.commandSet(Indexer.IndexerState.OFF, Indexer.IndexerState.OFF),
+                shooter.commandStop(),
+                new InstantCommand(() -> FeatherClient.recordShot(limelightDistance, lidarDistance, frontVelocity, rearVelocity, hoodState, profile))
         );
     }
 
