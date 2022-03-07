eba4b03cc5abfa57c6ee477e56774814fb359b56
Nate Sales <nate@natesales.net>
2022-03-06 17:21:09 -0800

    feat: add control diagram generation script

---
+++ generate-control-diagrams.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+npx chickencoop -i src/main/java/org/team1540/robot2022/RobotContainer.java -o controls.pdf
+npx chickencoop -i src/main/java/org/team1540/robot2022/utils/FeatherClient.java -o controls-feather.pdf
