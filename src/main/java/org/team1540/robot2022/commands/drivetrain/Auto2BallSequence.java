082a672dfe0ec16f37cfe71d80650a3dab48897b
Zach R <92497727+rutmanz@users.noreply.github.com>
2022-03-08 21:20:26 -0800

    feat: use sensors for shooting distance Not enough testing time for manual setpoints

--- src/main/java/org/team1540/robot2022/commands/drivetrain/Auto2BallSequence.java
+++ src/main/java/org/team1540/robot2022/commands/drivetrain/Auto2BallSequence.java
@@ -3,23 +3,31 @@ package org.team1540.robot2022.commands.drivetrain;
 import org.team1540.robot2022.commands.hood.Hood;
 import org.team1540.robot2022.commands.indexer.Indexer;
 import org.team1540.robot2022.commands.intake.Intake;
+import org.team1540.robot2022.commands.shooter.ShootSequence;
 import org.team1540.robot2022.commands.shooter.Shooter;
-import org.team1540.robot2022.commands.shooter.ShooterFeedSequence;
-import org.team1540.robot2022.utils.AutoHelper;
-import org.team1540.robot2022.utils.AutoSequence;
+import org.team1540.robot2022.utils.*;
 import org.team1540.robot2022.utils.AutoHelper.AutoPath;
-
 
 
 public class Auto2BallSequence extends AutoSequence {
 
-    public Auto2BallSequence(Drivetrain drivetrain, Intake intake, Indexer indexer, Shooter shooter, Hood hood, boolean isPosA) {
+    public Auto2BallSequence(Drivetrain drivetrain, Intake intake, Indexer indexer, Shooter shooter, Hood hood, Limelight limelight, LIDAR lidar, NavX navx, boolean isPosA) {
         AutoPath path = isPosA ? AutoPath.auto2Ball1A : AutoPath.auto2Ball1B;
         addPaths(path);
-        
+
         addCommands(
                 AutoHelper.runPathWithSpinup(drivetrain, intake, indexer, shooter, hood, path),  // Follow the path to collect the first ball
-                new ShooterFeedSequence(indexer, shooter)
+
+                new ShootSequence(shooter,
+                        indexer,
+                        drivetrain,
+                        hood,
+                        intake,
+                        limelight,
+                        lidar,
+                        navx,
+                        Shooter.ShooterProfile.FAR,
+                        false)
         );
     }
 }
